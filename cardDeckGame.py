7ce9348336954a04debf5e1efc0b938c26bee6fe
catherine Bose <catherinebose@catherines-MacBook-Pro.local>
2017-10-06 01:07:21 -0700

    Sucessfully executed creating deck, dealing, shuffling, drawfromDeck and show cards functionalitygit add .

--- cardDeckGame.py
+++ cardDeckGame.py
@@ -4,70 +4,76 @@ class Card(object):
     def __init__(self,suits,cardvalue):
         self.suits = suits
         self.cardvalue = cardvalue
-    
-        
-        
+
 class Deck(object):
-    def __init__(self):
+    def __init__(self,name):
         self.numcards = []
+        self.totalCardsAvailable = len(self.numcards)
+        self.deckName = name
     def shufflecards(self):
         shuffle(self.numcards)
-    def create_Deck(self):
+    def createDeck(self):
         suits=["spades","diamonds","hearts","clubs"]
         cardvalue=["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
         for i in range(0,4):
             for x in range(0,13):
-                card1 = Card(suits[i],cardvalue[x])
+                cardsInDeck = Card(suits[i],cardvalue[x])
                 # all cards are stored in num array
-                self.numcards.append(card1)
+                self.numcards.append(cardsInDeck)
                 # print "card1 Suits {} card1 value {}".format( card1.suits,card1.cardvalue)
         return self
     def deal(self,user):
-        print "We are Dealing now!!!"
+        print "Hi player",user.name
+        print "Total cards available in ",self.deckName,":",self.totalCardsAvailable
+        print "########We are Dealing now lets shuffle!!!##########"
+        self.shufflecards()
+        self.totalCardsAvailable = len(self.numcards) 
+        
         print "Each player gets 5 cards by default"
-        totalCardsAvailable = len(self.numcards) 
-        print "Total cards available:"
-        # print totalCardsAvailable
-        # for i in range (totalCardsAvailable,totalCardsAvailable-6,-1):
-        for i in range (0,6):
-            # print "numcards:", self.numcards
-            # print "self.numcards[i]: ", self.numcards[i].cardvalue, self.numcards[i].suits
-            user.hand.append(self.numcards[i])
-        # for x in range (1,6):
-        #     print "our hand consists of:"
-        #     print "value", user.hand[x].cardvalue, "suits", user.hand[x].suits
+        if(user.age<21):
+            print "We don't deal with minors ####Go back to kids zone###"
+        elif(self.totalCardsAvailable>=5 and user.totalCardsInHand<=0 and user.age>22):  
+            # for i in range (totalCardsAvailable,totalCardsAvailable-6,-1):
+            for i in range (1,6):
+                # user.hand.append(self.numcards[i])
+                user.hand.append(self.numcards.pop())
+                user.totalCardsInHand += 1
+                # print "Card added in hand is",user.hand[(len(user.hand))-1].suits,":",user.hand[(len(user.hand))-1].cardvalue
             return user.hand
 class Player(object):
     def __init__(self,name,age):
         self.name = name
         self.age = age
         self.hand = []
+        self.totalCardsInHand = 0
         # decklist is obj of deck
     def drawfromdeck(self,deckList):
-        for i in range (1, 6):
+        print "########Drawing a card from deck!!#######"
+        for i in range (1, 2):
             self.hand.append(deckList.pop())
-        for x in range (1,6):
-            print "Cards in my hand"
-            print "cardvalue:", self.hand[x].cardvalue
+            self.totalCardsInHand += 1
+            print "One card drawn from deck"
+            print "Total no of cards in hand is:", self.totalCardsInHand
+            print "You need to return a card back in lui of a card drawn"
+        return self
+        #for x in range (1,totalCardsInHand+1):
+        #     print "Cards in my hand"
+        #     print "cardvalue:", self.hand[x].cardvalue
     def showcardsinhand(self):
+        print "######Show Cards player {}#########".format(self.name)
         for x in range (0,5):
-            print "Show Cards"
-            print "cardvalue:", self.hand[x].cardvalue
-            print "Suit:", self.hand[x].suits
-            return self.hand
+            print "Suit: {} Card Value: {}".format(self.hand[x].suits,self.hand[x].cardvalue)
+            # print "cardvalue:", self.hand[x].cardvalue
+            # print "Suit:", self.hand[x].suits
+        return self
 
 # instance of deck
-deck1 = Deck()
-# create the deck function
-deck1.create_Deck()
+deck1 = Deck("deck1")
 # create a player
 player1 = Player("Phil","23")
-
-# Shuffle cards
-deck1.shufflecards()
-# deck will deal now
-deck1.deal(player1)
-# Player draws from deck
-player1.drawfromdeck(deck1.numcards)
+# create a deck and deal
+deck1.createDeck().deal(player1)
 # Players shows the cards
 player1.showcardsinhand()
+# Player draws from deck
+player1.drawfromdeck(deck1.numcards)
